241c3447a7a46faf36dbb9fe67a77bb24e413c49
ciupa.kamil <194928@edu.p.lodz.pl>
2017-05-12 09:40:16 -0700

    a

---
+++ app/src/main/java/kamil/ciupa/astrotime/MainActivity.java
@@ -0,0 +1,113 @@
+package kamil.ciupa.astrotime;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.astrocalculator.AstroCalculator;
+
+public class MainActivity extends AppCompatActivity {
+
+    AstroCalculator.Location location;
+    double latitude;
+    double longitude;
+    boolean isDialogOn = false;
+    AlertDialog b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        latitude = 10.00;
+        longitude = 10.00;
+        location = new AstroCalculator.Location(latitude,longitude);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+           // return true;
+            showOptionsDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showOptionsDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.options_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        dialogBuilder.setTitle("Custom dialog");
+        dialogBuilder.setMessage("Enter text below");
+
+        b = dialogBuilder.create();
+
+        Button a = (Button) dialogView.findViewById(R.id.bOK);
+        EditText lt = (EditText) dialogView.findViewById(R.id.LatitudeET);
+        EditText lg = (EditText) dialogView.findViewById(R.id.LongitudeET);
+        lt.setText(Double.toString(latitude));
+        lg.setText(Double.toString(longitude));
+
+        a.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                latitude = Double.parseDouble( lt.getText().toString());
+                b.dismiss();
+            }
+        });
+
+
+
+
+
+        b.show();
+
+
+    }
+
+
+
+
+
+
+   }
+
